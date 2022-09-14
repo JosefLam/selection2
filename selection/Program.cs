1a12f0e7d36dd209ea726e2176348e2997edf615
Josef Lambley <17LamblJ@fulford.york.sch.uk>
2022-09-14 12:25:51 +0100

    added function

--- selection/Program.cs
+++ selection/Program.cs
@@ -8,17 +8,43 @@ namespace selection
 {
     class Program
     {
+        /// <summary>
+        /// ask a question
+        /// </summary>
+        /// <param name="question"><the question to ask/param>
+        /// <returns>the asnwer to the question</returns>
+        private static string input(string question)
+        {
+            Console.Write(question);
+            return Console.ReadLine();
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("whivh topping?");
-            string pizza = Console.ReadLine().ToLower();
+
+            string pizza = input ("what pizza would u like");
 
             switch (pizza)
             {
                 case "cheese and tomato":
-                    Console.WriteLine();
+                    Console.WriteLine("vegetarian");
+                    break;
             }
+
+            string music = input("what type of music do you like");
+            switch(music)
+            {
+                case ("rock"):
+                    Console.WriteLine("response");
+                    break;
+            }
+
+
             Console.ReadLine();
+            
+
+
         }
     }
 }
